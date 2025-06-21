7885e8f45aa98c0217120582a316ed15fb559c58
Slava Olesik <looksof@gmail.com>
2025-06-21 09:31:35 +0300

    Update curl-example.sh

--- examples/curl-example.sh
+++ examples/curl-example.sh
@@ -1,3 +1,1 @@
-curl \
-  "https://netapi.com/api2/?method=compromised&dataset_type=url" \
-  --output compromized.csv
+curl "https://netapi.com/api2/?method=download&zone_tld=net&dataset_type=list&filter_type=active&token=YOUR_API_KEY" -o domains_net.csv.gz
